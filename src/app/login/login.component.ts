c3cc0593d5abf2ce7d32cefbb6d11377babb7f07
sahalsaman <sahalsamankc@gmail.com>
2022-04-06 11:39:31 +0530

    firstCommit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  aim="your perfect bak partner"
+  accn="Enter account number"
+  acno=""
+  pswd=""
+
+  dataBase={1001:{acno:1001,uname:"sahal",password:1001,balance:100000},
+            1002:{acno:1001,uname:"sahal",password:1001,balance:100000},
+            1003:{acno:1001,uname:"sahal",password:1001,balance:100000}}
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  acnoChange(event:any){
+    // console.log(event.target.value);
+    this.acno=event.target.value
+  }
+  pswdChange(event:any){
+    // console.log(event.target.value);
+    this.pswd=event.target.value
+  }
 
+ login(){
+   alert("account number"+this.acno+"\npassword"+this.pswd)
+   
+ }
 }
