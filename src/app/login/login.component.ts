9a8674ec851de72e8e60d0eaceda7a37d18fd684
sahalsaman <sahalsamankc@gmail.com>
2022-04-06 12:42:37 +0530

    template referencing Commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,28 +20,46 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  acnoChange(event:any){
-    // console.log(event.target.value);
-    this.acno=event.target.value
-  }
-  pswdChange(event:any){
-    // console.log(event.target.value);
-    this.pswd=event.target.value
-  }
+//        Event Binding using $event
+//  ==========================
+//   acnoChange(event:any){
+//     // console.log(event.target.value);
+//     this.acno=event.target.value
+//   }
+//   pswdChange(event:any){
+//     // console.log(event.target.value);
+//     this.pswd=event.target.value
+//   }
 
- login(){
-   var acno=this.acno
-   var pswd=this.pswd
-   var dataBase=this.dataBase
-   if(acno in dataBase){
-     if(pswd == dataBase[acno]["password"]){
-       alert("login successfull")
+//  login(){
+//    var acno=this.acno
+//    var pswd=this.pswd
+//    var dataBase=this.dataBase
+//    if(acno in dataBase){
+//      if(pswd == dataBase[acno]["password"]){
+//        alert("login successfull")
+//      }else{
+//        alert("invalid password")
+//      }
+//    }else{
+//      alert("user does not exist")
+//    }
+//  }
+
+//   Template Refernce variable
+// =====================
+login(a:any,p:any){
+     var acno=a.value
+     var pswd=p.value
+     var dataBase=this.dataBase
+     if(acno in dataBase){
+       if(pswd == dataBase[acno]["password"]){
+         alert("login successfull")
+       }else{
+         alert("invalid password")
+       }
      }else{
-       alert("invalid password")
+       alert("user does not exist")
      }
-   }else{
-     alert("user doues not exist")
    }
-   
- }
 }
